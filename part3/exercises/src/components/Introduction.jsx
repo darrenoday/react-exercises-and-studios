d544d4f9cbd1175842778c39471cd23cadadd14a
Darren Oday <darren.oday@gmail.com>
2024-03-19 21:20:22 -0500

    finished exercise

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About foraging</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>you can find food in the woods </li>
+            <li>mushrooms, berrties, fruits etc</li>
+            <li>careful though you could eat poison.</li>
          </ol>
       </div>
    );
